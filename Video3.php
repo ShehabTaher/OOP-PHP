6925b10f307bac66ba507707d2298f53834eba86
Shehab Taher <shehabyaher@outlook.com>
2022-06-30 02:01:41 +0200

    first commit

---
+++ Video3.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+ #3
+    [1] Class  : Is A Blueprint that u can create object from
+    [2] Object : Is A Member In the main Application
+    [3] Examples :
+                   (1) Apple :
+                            -> Class       = Apple Blueprint Design
+                            -> Object      = iPhone that China Made
+                            -> Application = Apple Store
+                    (1) Web Applicatin Registration :
+                            -> Class       = Code to Add new member
+                            -> Object      = The Members
+                            -> Application = Web Applicatin Registration
+
+
+*/
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+
+ #3
+    [1] Class  : Is A Blueprint that u can create object from
+    [2] Object : Is A Member In the main Application
+    [3] Examples :
+                   (1) Apple :
+                            -> Class       = Apple Blueprint Design
+                            -> Object      = iPhone that China Made
+                            -> Application = Apple Store
+                   (2) Web Applicatin Registration :
+                            -> Class       = Code to Add new member
+                            -> Object      = The Members
+                            -> Application = Web Applicatin Registration
+                   (3) Blog system :
+                            -> Class       = Code to Add new Post , Articale , News , Infomartion
+                            -> Object      = Post , Articale , News , Infomartion
+                            -> Application = Blog system
+
+*/
+
+class AppleDevice {
+   //Properties
+   public $ram;
+   public $inch;
+   public $space;
+   public $color;
+}
+
+$iphone6s = new AppleDevice();
+$iphone7 = new AppleDevice();
+
+echo "<pre>";
+var_dump($iphone6s);
+echo "</pre>";
+echo "<pre>";
+var_dump($iphone7);
+echo "</pre>";
